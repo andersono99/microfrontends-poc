4f889bcde0d7641ffa6d5c872980f6b09077477f
andersono99 <42416569+andersono99@users.noreply.github.com>
2021-05-13 21:50:44 -0500

    commit agg microfronteds

---
+++ root-config-app/src/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { LifeCycles, registerApplication, start } from "single-spa";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+registerApplication({
+  name: "app-visualizar",
+  app: (): Promise<LifeCycles> => (window as any).System.import("@single-spa-test/app-visualizar"),
+  activeWhen: "/visualizar"
+});
+
+registerApplication({
+  name: "app-pagar",
+  app: (): Promise<LifeCycles> => (window as any).System.import("@single-spa-test/app-pagar"),
+  activeWhen: "/pagar"
+});
+
+registerApplication({
+  name: "app-listar",
+  app: (): Promise<LifeCycles> => (window as any).System.import("@single-spa-test/app-listar"),
+  activeWhen: "/listar"
+});
+
+start();
